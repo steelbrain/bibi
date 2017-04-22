6c5c492db3daaccd4988086e50e1be31492d3e8a
Daniel Shneyder <archcorsair@gmail.com>
2017-04-22 12:35:54 -0700

    :new: await the Promises

--- src/commands/sync.js
+++ src/commands/sync.js
@@ -10,14 +10,14 @@ export default class SyncCommand extends Command {
   async callback() {
     const projects = await this.getProjects()
     if (!projects.length) {
-      throw new CLIError('You don\'t have any projects to sync')
+      throw new CLIError("You don't have any projects to sync")
     }
     const promises = []
     const syncRepo = project => this.spawn('git', ['fetch', '--all'], { stdio: 'inherit', cwd: project.path })
     projects.map((project) => {
       promises.push(syncRepo(project))
     })
-    Promise.all(promises).then((results) => {
+    await Promise.all(promises).then((results) => {
       console.log('results', results)
       return results
     }).catch(e => new CLIError('Something went wrong syncing your repos:', e))
