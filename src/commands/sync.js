c0b01a2a42d3f538a10748be5d7f5740a5c74ebb
Daniel Shneyder <archcorsair@gmail.com>
2017-04-22 12:22:08 -0700

    :new: Sync all existing repos with Sync command

--- src/commands/sync.js
+++ src/commands/sync.js
@@ -1,12 +1,28 @@
 /* @flow */
 
 import Command from '../command'
+import { CLIError } from '../common'
 
 export default class SyncCommand extends Command {
   name = 'sync'
   description = 'Sync upstream changes'
 
-  callback() {
-    console.log('sync', this.projectsRoot)
+  async callback() {
+    const projects = await this.getProjects()
+    if (!projects) {
+      throw new CLIError('You don\'t have any projects to sync')
+    }
+    const promises = []
+    const syncRepo = async (project) => {
+      await this.spawn('git', ['fetch', '--all'], { stdio: 'inherit', cwd: project.path })
+    }
+    projects.map((project) => {
+      promises.push(syncRepo(project))
+    })
+
+    Promise.all(promises).then((results) => {
+      console.log('results', results)
+      return results
+    }).catch(e => new CLIError('Something went wrong syncing your repos:', e))
   }
 }
