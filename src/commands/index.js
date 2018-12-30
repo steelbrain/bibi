1809eee268d2018104e9b049c28173a4cecaf5a7
Steel Brain <i@steelbrain.me>
2018-12-30 14:55:50 +0500

    :fire: Remove the import command

--- src/commands/index.js
+++ src/commands/index.js
@@ -1,17 +1,15 @@
 /* @flow */
 
-import GetCommand from './get'
-import ExecCommand from './exec'
-import SyncCommand from './sync'
-import ImportCommand from './import'
-import GetAllCommand from './get-all'
-import GetProjectsRootCommand from './get-projects-root'
+import GetCommand from "./get";
+import ExecCommand from "./exec";
+import SyncCommand from "./sync";
+import GetAllCommand from "./get-all";
+import GetProjectsRootCommand from "./get-projects-root";
 
 export default [
   GetCommand,
   GetAllCommand,
   ExecCommand,
   SyncCommand,
-  ImportCommand,
-  GetProjectsRootCommand,
-]
+  GetProjectsRootCommand
+];
